53a560916deda881d851368810a3f8bd0bc5a3c9
Xatan01 <xaviertan2001@gmail.com>
2025-10-24 15:56:23 +0800

    formatting changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import ClientList from "./components/client/ClientList";
-import ClientPortal from "./components/client/ClientPortal";
-import AgentDashboard from "./components/dashboard/AgentDashboard";
-import NotificationSystem from "./components/notifications/NotificationSystem";
-import SubscriptionManagement from "./components/subscription/SubscriptionManagement";
-import TimelineBuilder from "./components/timeline/TimelineBuilder";
-import Home from "./components/home";
 
 // ✅ Auth pages
-import Register from "./components/register/register";
-import Login from "./components/login/login";
-import Confirm from "./components/confirm/confirm";
-import ForgotPassword from "./components/ForgetPassword/ForgetPassword";
-import ResetPassword from "./components/ResetPassword/ResetPassword";
+import Register from "@/pages/auth/register";
+import Login from "@/pages/auth/login";
+import ResetPassword from "@/pages/auth/ResetPassword";
+import ForgetPassword from "@/pages/auth/ForgetPassword";
+
+// ✅ Agent / Client pages
+import AgentDashboard from "@/pages/agents/AgentDashboard";
+import TimelineBuilder from "@/pages/agents/TimelineBuilder";
+import Home from "@/pages/agents/home";
+
+
 
 function App() {
   return (
@@ -21,16 +20,12 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/agent-login" element={<Login />} />
       <Route path="/client-login" element={<Login />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/forgot-password" element={<ForgetPassword />} />
       <Route path="/reset-password" element={<ResetPassword />} />
 
       {/* App routes */}
       <Route path="/" element={<Login />} />
-      <Route path="/client-list" element={<ClientList />} />
-      <Route path="/client-portal" element={<ClientPortal />} />
       <Route path="/dashboard" element={<AgentDashboard />} />
-      <Route path="/notifications" element={<NotificationSystem />} />
-      <Route path="/subscription" element={<SubscriptionManagement />} />
       <Route path="/timeline" element={<TimelineBuilder />} />
     </Routes>
   );
